879bb5c64d9f9d8c8575937bb019c2728ecd6e53
soumik <sr2211299@gmail.com>
2025-12-29 17:21:33 +0530

    CRUD done

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -17,40 +17,101 @@
 
 import todoSchema from "../models/todoSchema.js"
 //create a todo
-export const createTodo = async(req, res)=>{
+export const createTodo = async (req, res) => {
     try {
-        const {title} = req.body
-        const data = await todoSchema.create({title})
+        const { title } = req.body
+        const data = await todoSchema.create({ title })
         return res.status(201).json({
-            success:true,
-            message:"Todo created successfully",
-            data:data
+            success: true,
+            message: "Todo created successfully",
+            data: data
         })
-        
+
     } catch (error) {
-          return res.status(500).json({
-            success:false,
-            message:"Todo not created "
+        return res.status(500).json({
+            success: false,
+            message: "Todo not created "
         })
-        
+
     }
 }
 
 //read all todos
-export const getAllTodo = async(req, res)=>{
+export const getAllTodo = async (req, res) => {
     try {
         const data = await todoSchema.find({})
         return res.status(200).json({
-            success:true,
-            message:"Todo fetched successfully",
-            data:data
+            success: true,
+            message: "Todo fetched successfully",
+            data: data
         })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Todo not fetched "
+        })
+
+    }
+}
+
+
+//delete a  todo
+export const delTodo = async (req, res) => {
+    try {
+        const todoId = req.params.id
+        const data = await todoSchema.findByIdAndDelete({ _id: todoId })
+        if (data) {
+            return res.status(200).json({
+                success: true,
+                message: "Todo deleted successfully",
+                data: data
+            })
+        }
+        else {
+            return res.status(404).json({
+                success: false,
+                message: "Todo not found"
+            })
+
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Todo not deleted "
+        })
+    }
+}
+
+
+//update a todo
+export const updateTodo = async(req, res)=>{
+    try {
+        const {title} = req.body
+        const todoId = req.params.id
+
+        const data = await todoSchema.findOne({_id:todoId})
+        if(!data){
+             return res.status(404).json({
+                success: false,
+                message: "Todo not found"
+            })
+        }
+        data.title = title
+        await data.save()
+
+         return res.status(200).json({
+                success: true,
+                message: "Todo updated successfully",
+                data: data
+            })
         
     } catch (error) {
-          return res.status(500).json({
-            success:false,
-            message:"Todo not fetched "
-        })
+         return res.status(500).json({
+                success: false,
+                message: "Todo not updated"
+            })
         
     }
 }
--- src/routes/todoRoute.js
+++ src/routes/todoRoute.js
@@ -1,9 +1,11 @@
 
 import express from 'express'
-import { createTodo, getAllTodo } from '../controllers/todoController.js'
+import { createTodo, delTodo, getAllTodo, updateTodo } from '../controllers/todoController.js'
 const todoRoute = express.Router()
 
 todoRoute.post('/create', createTodo) //api = func + url
 todoRoute.get('/getAll', getAllTodo) 
+todoRoute.delete('/delete/:id', delTodo) 
+todoRoute.put('/update/:id', updateTodo) 
 
 export default todoRoute
